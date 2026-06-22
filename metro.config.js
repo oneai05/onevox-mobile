e72aa4f666d9a55b4054ffa3c1990068f4abc2a6
asafecesar95 <asafe.cesar@unifesp.br>
2026-06-22 10:50:13 -0300

    Corrigir cache NativeWind no build web

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,13 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+config.watchFolders = [
+  ...(config.watchFolders || []),
+  path.resolve(__dirname, "node_modules/react-native-css-interop/.cache"),
+];
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
